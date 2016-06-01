c2b7f84ebf1a7b82b6ab9b3baee23fd1f424ca29
zj <zj@qq.com>
2016-06-01 18:53:31 +0800

    提供orm文档说明，并找到其中一个BUG

--- backends/orm.py
+++ backends/orm.py
@@ -4,6 +4,36 @@
 
 """
 ORM的实现
+Field:
+    1. count主要用于 Models字段在数据表的字段顺序
+    2. ddl 字段描述 如: varchar(255) 或 int 或 double
+    3. default 可以传入函数或其他
+
+ModelMetaclass:
+    1. 用于控制Model的子类的生成方式
+    2. 将Model的子类中属性字段是Filed type的pop出，放入__mappings__中
+        如：
+        class User(Model):
+            name = CharField()
+            age  = IntegerField()
+        a = User()
+        此时， a具有__mappings__属性 为{'name':<xx.CharField xx>, 'age': <xx.IntegerField xx>}
+    3. __sql__ 一个匿名函数，用以生成SQL语句
+       默认添加主键 'id int auto_increment primary key'
+       可以通过 print a.__sql__() 查看语句
+       也可以 db.update(a.__sql__())直接创建表
+    4. __table__ 用以设置User的表名，若在定义中未设置，则默认为类名的小写
+
+Model:
+    1. 继承于dict, 改写__getattr__和__setattr__
+    2. 类方法:
+        cls.get(**kw): 返回满足要求的第一条记录，并将其转换为对应 cls 的instance 或 None
+        cls.filter(**kw): 返回一组cls instance 或 []
+        cls.create(**kw): 调用self.insert()返回 cls instance  // 有BUG
+    3. insert(self):
+        插入数据库, 由于默认添加主键, 插入之后，执行查找, 然后self.id = id // 有BUG 
+    4. update(self): 对数据更新
+    5. delte(self): 删除数据库中记录
 """
 
 
@@ -91,7 +121,7 @@ class ModelMetaclass(type):
             if isinstance(v, Field):
                 mappings[k] = attrs.pop(k)
         attrs['__mappings__'] = mappings
-        attrs['__primary_key__'] = 'id'
+        # attrs['__primary_key__'] = 'id'
         if not attrs.get('__table__'):
             attrs['__table__'] = name.lower()
         attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
@@ -142,6 +172,16 @@ class Model(dict):
         return cls(**d)
 
     def insert(self):
+        """
+        有BUG,
+        1. 新建conn, 插入, conn.commit(), conn.close()
+        2. 新建conn, 查询, conn.commit(), conn.close() filter出最后一个
+        若在这两次conn中，有其他conn插入同样的值, 此时filter出来的数据就错误了
+
+        解决方法:
+            在db中添加新方法
+            新建conn, 插入, 查询, conn.commit(), conn.close()
+        """
         params = {}
         for k, v in self.__mappings__.items():
             if not hasattr(self, k):
@@ -149,6 +189,7 @@ class Model(dict):
             params[k] = getattr(self, k)
         db.insert(self.__table__, **params)
         d = self.filter(**params)[-1]
+        
         self.id = d.id
 
     def update(self):
@@ -177,5 +218,6 @@ if __name__ == '__main__':
         title = CharField()
         create_time = TimeField()
         content = TextField()
-        
-    b = Blog(title='123',create_time=1.2, content='info')
+    b = Blog()
+    print b.__sql__()
+    print b.__table__
